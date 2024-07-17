fc70982e763187b4cf88c82f64b8133556a76d42
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2024-07-18 00:57:26 +0200

    feat: implement basic home page

--- calendartodolist.client/src/views/HomeView.vue
+++ calendartodolist.client/src/views/HomeView.vue
@@ -2,5 +2,14 @@
 </script>
 
 <template>
-    Home
+    <v-container>
+        <v-row class="justify-center my-10">
+            <v-col cols="12" class="text-center justify-center">
+                <v-img src="/logo.png" max-width="200" class="my-4 ml-auto mr-auto"></v-img>
+                <h1 class="text-h2 my-4">Welcome to Calendar</h1>
+                <p class="subtitle-1">Your ultimate planner application</p>
+            </v-col>
+        </v-row>
+
+    </v-container>
 </template>
