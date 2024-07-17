4a2f13a9f215592c0754c347ebdf0473ae2bc4a3
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2024-07-17 18:30:08 +0200

    feat: add vuetify

--- calendartodolist.client/src/main.ts
+++ calendartodolist.client/src/main.ts
@@ -1,9 +1,27 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import App from './App.vue'
 import router from './router'
 
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+})
+
 createApp(App)
+    .use(vuetify)
     .use(router)
     .mount('#app')
