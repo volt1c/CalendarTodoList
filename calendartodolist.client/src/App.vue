49755ad7afafd7c5cabeaba16f4944bce634920b
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2024-07-18 12:50:41 +0200

    feat: improve layout

--- calendartodolist.client/src/App.vue
+++ calendartodolist.client/src/App.vue
@@ -1,21 +1,41 @@
 <script setup lang="ts">
-    import LogoImg from '@/components/LogoImg.vue'
+import LogoImg from '@/components/LogoImg.vue'
+import { useStore } from 'vuex';
+import { key } from './stores/auth';
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+const store = useStore(key)
+
+console.log('isAuthorized', store.getters.isAuthorized)
+
+const handleLogout = () => {
+    store.commit('storeClear')
+    router.push('/')
+}
 </script>
 
 <template>
     <v-app>
         <v-layout>
             <v-app-bar>
-                <v-app-bar-title @click="$router.push('/')">
+                <v-app-bar-title @click="$router.push('/')" :style="{ minWidth: '120px' }">
                     <v-icon start>
-                        <logo-img/>
+                        <logo-img />
                     </v-icon>Calendar
                 </v-app-bar-title>
-                <v-spacer></v-spacer>
-                <v-btn color="primary"
-                       variant="flat"
-                       class="mr-2"
-                       to="/login">
+
+                <v-spacer class="min-w-0"></v-spacer>
+
+                <v-btn v-if="store.getters.isAuthorized" color="primary" icon="mdi-calendar" variant="text" class="mr-2"
+                    to="/calendar"></v-btn>
+
+                <v-btn v-if="store.getters.isAuthorized" color="primary" variant="outlined" class="mr-2"
+                    v-on:click="handleLogout">
+                    Logout
+                    <v-icon icon="mdi-login" end></v-icon>
+                </v-btn>
+                <v-btn v-else color="primary" variant="flat" class="mr-2" to="/login">
                     Login
                     <v-icon icon="mdi-login" end></v-icon>
                 </v-btn>
