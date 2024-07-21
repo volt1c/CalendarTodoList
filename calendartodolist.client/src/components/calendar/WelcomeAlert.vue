9efca11ac6523938439e1ae741cb888554099f5d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2024-07-22 01:24:36 +0200

    refactor: calendar view --wip--

---
+++ calendartodolist.client/src/components/calendar/WelcomeAlert.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const model = defineModel({ default: true })
+</script>
+
+<template>
+    <v-alert v-model="model" border="start" close-label="Close Alert" color="deep-purple-accent-4" class="mx-2"
+        title="Welcome 🎉" variant="tonal" closable>
+        We are glad that you decided to use our solution. We will take care of it to make using Calendar more
+        and more convenient over time.
+    </v-alert>
+</template>
